6c6f09d85327b973364e50fa2da8ca1e5d41aeb2
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2018-04-16 17:33:11 -0400

    made this more like a note book app. added a sidebar to navigate to different notes with links. Added reverse option to order posts in chronological order or reveresed.

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
-# Python static website generator 
+# Markdown converter and joiner
 
-a project to write blogs or create static websites in markdown.
-
-There are plenty of other ways to do this (hugo/jekyll) and maybe there is a project just like this but I'm just making to learn some more skills. Other projects will be much more robust but this works for what I wanted to do and is a very small amount of code.
-
-Just write in a text editor/vim (using markdown if you want html) and save your file into the `posts` directory.
+Convert markdown files to html and join them together 
 
 ## Usage
-Create a `posts` directory in the same directory as blog.py if `posts` does not yet exist.
-
-When you are ready to regenerate your blog just run:
-`python blog.py --generate`
-
-Everytime a new file is added to the `posts` directory you will have to re-enter author, title, description... because its not that much work to just do it and I didn't want to add a database or another solution.
+`python generate_site.py [directory]`
 
 ## Styling
 if `style.css` does not exist in the `css` directory, create it.
 
 Edit the `style.css` file in the `css` directory with any styling you want. Skeleton CSS is included by default. Add your own styling on top of it.
-
 
 ### Skeleten CSS
 By default going to include skeleton css files in a css directory and use that as the styling. I'm not planning to add more but it could be done if wanted.
@@ -28,7 +18,3 @@ This is a mobile friendly styling so it should fit to any screen.
 ## Dependencies
 1. mistune (converting markdown to html)
 2. tbd
-
-
-## To Do
-2. instructions to setup with github pages so when its committed it uploads there.
---
+++ generate_site.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Combine your markdown files into a single html file
+
+dependencies:
+    - mistune
+"""
+import os
+import sys
+import mistune  # pip install mistune
+
+if __name__ == "__main__":
+    if sys.argv[1] == 'help':
+        print('join markdown files into single html file')
+        print('usage: python generate_site.py [directory]')
+        exit()
+    markdown_text = "\n"
+    file_id = 0
+    dir = os.listdir(sys.argv[1])
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "reverse":
+            dir.reverse()
+    if os.path.isdir(sys.argv[1]):
+        for f in dir:
+            if os.path.isfile(os.path.join(sys.argv[1], f)) and f.endswith('.md'):
+                try:
+                    with open(os.path.join(sys.argv[1], f), 'r') as f:
+                        markdown_text += "\nreplace_me{}\n".format(file_id)
+                        markdown_text += f.read()
+                        markdown_text += "\nclose_me\n"
+                        markdown_text += '\n---\n'
+                        file_id += 1
+                except Exception as e:
+                    print(e)
+    markdown = mistune.Markdown()  # default settings
+    html_formatted = markdown(markdown_text)
+    for i in range(0, file_id):
+        replace_me = "<p>replace_me{}</p>".format(i)
+        replacement = "<div id=file_id{}>".format(i)
+        html_formatted = html_formatted.replace(replace_me, replacement)
+    html_formatted = html_formatted.replace("<p>close_me</p>", "</div>")
+    html_formatted = "<div class=container style='font-family: verdana;'>" \
+                     + html_formatted + "</div>"
+    # build sidebar navigation
+    side_bar = "<div class='two columns sidebar' style='font-family: verdana; float: left; height: 900px; position: fixed; border-right: 1px solid #E1E1E1;'><br>"
+    side_bar += "<h2>Notes</h2><ul style='list-style-type: none;'>"
+    for i in range(0, file_id):
+        side_bar += "<li><a href=#file_id{0} style='text-decoration: none;'>Note {0}</a></li>".format(i)
+    side_bar += "</ul>"
+    side_bar += "</div><br>"
+    html_formatted = side_bar + html_formatted
+    html_formatted = '<link href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" rel="stylesheet"></link> ' + html_formatted
+    name = 'static_site_gen.html'
+    name = os.path.join(sys.argv[1], name)
+    with open(name, 'w') as f:
+        f.write(html_formatted)
+    print(name)
